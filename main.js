2d4b1bb2f5d738d95dac8c96a14f0d8a5c191493
tsvetislav <tsvetislavt99@gmail.com>
2022-01-05 10:27:34 +0200

    added toggle for mobile/small tablet screen

---
+++ main.js
@@ -0,0 +1,11 @@
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () => {
+  const isVisible = primaryNav.getAttribute('data-visible');
+  if (isVisible === 'false') {
+    primaryNav.setAttribute('data-visible', 'true');
+  } else {
+    primaryNav.setAttribute('data-visible', 'false');
+  }
+});
